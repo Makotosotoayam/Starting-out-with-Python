8a0a3421ba32a18a6450caa05c1967e88840c509
Makotosotoayam <rnafis98@gmail.com>
2024-10-01 10:49:18 +0700

    square

--- square.py
+++ square.py
@@ -7,6 +7,6 @@ s = int(input("Insert the value here : "))
 Area = s * s
 Perimeter = 4 * s
 
-print("The results of the Area is\t: "(Area))
-print("The results of the Perimeter is\t: "(Perimeter))
+print("The results of the Area is\t: ",(Area))
+print("The results of the Perimeter is\t: ",(Perimeter))
 
