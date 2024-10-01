ab0fe01fbd4d194f6d32fc5a15f17d6ee45e7d37
Makotosotoayam <rnafis98@gmail.com>
2024-10-01 10:40:32 +0700

    square

---
+++ square.py
@@ -0,0 +1,12 @@
+print("="*25)
+print("Calculating A Square")
+print("="*25)
+
+s = int(input("Insert the value here : "))
+
+Area = s * s
+Perimeter = 4 * s
+
+print("The results of the Area is\t: "(Area))
+print("The results of the Perimeter is\t: "(Perimeter))
+
