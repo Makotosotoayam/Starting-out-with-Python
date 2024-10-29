74d90053506c6a2db08f91f7155a3033ef9b4f98
Makotosotoayam <rnafis98@gmail.com>
2024-10-29 10:17:21 +0700

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,14 @@
+print("="*25)
+print(Calculating A Triangle
+print("="*25)
+
+b = float(input("Insert the base value\t : ")
+s1 = float(input("Insert the side 1 value\t : ")
+s2 = float(input("Insert the side 2 value\t : ")
+h = float(input(" Insert the height value\t : ")
+
+area = 1/2 * b * h
+perimeter = s1 + s2 + b
+
+print("The result of the area : ",round(area))
+print(" The result of the perimeter : ",round(perimeter))
