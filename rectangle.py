8b8cd172e31be9707dc15e3dd6622ed0083e77ca
Makotosotoayam <rnafis98@gmail.com>
2024-10-29 10:33:33 +0700

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+print("="*25)
+print("Calculating A Rectangle")
+print("="*25)
+
+w = float(input("Insert the width value : "))
+l = float(input("Insert the length value : "))
+
+area = l * w
+perimeter = (2 * w) + (2 * l)
+
+print("The result of the area : ",round(area))
+print("The result of the perimeter : ",round(perimeter))
