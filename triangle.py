76eb090e0ae9f0dd8a09195ebc1b78a5cb9e99c4
Makotosotoayam <rnafis98@gmail.com>
2024-10-29 10:41:05 +0700

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -2,10 +2,10 @@ print("="*25)
 print("Calculating A Triangle")
 print("="*25)
 
-b = float(input("Insert the base value\t : ")
-s1 = float(input("Insert the side 1 value\t : ")
-s2 = float(input("Insert the side 2 value\t : ")
-h = float(input(" Insert the height value\t : ")
+b = float(input("Insert the base value\t : "))
+s1 = float(input("Insert the side 1 value\t : "))
+s2 = float(input("Insert the side 2 value\t : "))
+h = float(input("Insert the height value\t : "))
 
 area = 1/2 * b * h
 perimeter = s1 + s2 + b
