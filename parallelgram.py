8834aa2cd6832f3a1bdead583eb93f1e8dae3a93
Makotosotoayam <rnafis98@gmail.com>
2024-10-29 10:39:39 +0700

    parallelgram.py

---
+++ parallelgram.py
@@ -0,0 +1,13 @@
+print("="*25)
+print("Calculating A Parallelgram")
+print("="*25
+
+w = float(input("Insert the width : "))
+l = float(input("Insert the length : "))
+h = float(input(" Insert the height : "))
+
+area = l * h
+perimeter = (2 * l) + (2 * w)
+
+print("The result of the area : ",round(area))
+print("The result of the perimeter : ",round(perimeter))
