497069df31a4f7a1989a69323d97a5fb42bfd608
Makotosotoayam <rnafis98@gmail.com>
2024-11-01 09:46:25 +0700

    Update cuboid(balok).py

--- cuboid(balok).py
+++ cuboid(balok).py
@@ -6,12 +6,12 @@ p = float(input("input the length\t: "))
 l = float(input("input the width\t: "))
 t = float(input("input the height\t: "))
 
-surface = lambda p,l,t: 2 * (p * l) + (p * t) + (l * t)
-volume = lambda p,l,t: (p * l * t)
-area = lambda p,l,t: 4 * (p + l + t)
+surface = 2 * (p * l) + (p * t) + (l * t)
+volume = p * l * t
+area = 4 * (p + l + t)
 
-print(f"LUAS\t\t: {surface(p,l,t)} cm2")
-print(f"VOLUME\t\t: {volume(p,l,t)} cm2")
-print(f"AREA\t: {area(p,l,t)} cm2")
+print("the result of surface\t: ",round(surface))
+print("the result of volume\t: ",round(volume))
+print("the result of area\t: ",round(area))
 
 
