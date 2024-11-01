d3f4c9e890a6fbff6e3c8890e7e7388330a30a50
Makotosotoayam <rnafis98@gmail.com>
2024-11-01 09:40:15 +0700

    Create Circle.py

---
+++ Circle.py
@@ -0,0 +1,12 @@
+print('='*25)
+print('Calculating A Circle')
+print('='*25)
+
+r = float(input('insert the radius : '))
+pi = 3.14
+
+diameter = 2 * r
+area = pi * r * 2
+
+print('the result of diameter',round(diameter))
+print('the result of area',round(area))
